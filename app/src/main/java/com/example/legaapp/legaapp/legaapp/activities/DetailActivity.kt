1f656f375981a9b126503b2ca494d27ef3a80bb6
mariathathiana <109003131+mariathathiana@users.noreply.github.com>
2025-10-02 10:14:01 +0200

    ghg
    kgkug

--- app/src/main/java/com/example/legaapp/legaapp/legaapp/activities/DetailActivity.kt
+++ app/src/main/java/com/example/legaapp/legaapp/legaapp/activities/DetailActivity.kt
@@ -10,12 +10,15 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.legaapp.R
+import com.example.legaapp.legaapp.legaapp.data.Lega
+import com.example.legaapp.legaapp.legaapp.utils.SessionManager
 
 class DetailActivity : AppCompatActivity() {lateinit var nameTextView: TextView
     lateinit var datesTextView: TextView
     lateinit var iconImageView: ImageView
 
-    lateinit var horoscope: Horoscope
+    lateinit var Lega : Lega
     lateinit var session: SessionManager
     lateinit var favoriteMenu: MenuItem
     var isFavorite = false
@@ -36,19 +39,19 @@ class DetailActivity : AppCompatActivity() {lateinit var nameTextView: TextView
         datesTextView = findViewById(R.id.datesTextView)
         iconImageView = findViewById(R.id.iconImageView)
 
-        val id = intent.getStringExtra("HOROSCOPE_ID")!!
+        val id = intent.getStringExtra("LEGA_ID")!!
 
         isFavorite = session.isFavorite(id)
 
-        horoscope = Horoscope.getById(id)
+        lega = Lega.getById(id)
 
-        nameTextView.setText(horoscope.name)
-        datesTextView.setText(horoscope.dates)
-        iconImageView.setImageResource(horoscope.sign)
+        nameTextView.setText(lega.name)
+
+        iconImageView.setImageResource(lega.sign)
 
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
-        supportActionBar?.setTitle(horoscope.name)
-        supportActionBar?.setSubtitle(horoscope.dates)
+        supportActionBar?.setTitle(lega.name)
+
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
@@ -65,7 +68,7 @@ class DetailActivity : AppCompatActivity() {lateinit var nameTextView: TextView
             R.id.action_favorite -> {
                 isFavorite = !isFavorite
                 if (isFavorite) {
-                    session.setFavorite(horoscope.id)
+                    session.setFavorite(legaid)
                 } else {
                     session.setFavorite("")
                 }
